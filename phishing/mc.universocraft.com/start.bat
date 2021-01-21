265d149f02a779517b080cd5d0a0990653aedd6e
Pedro Vega <67062049+wrrulos@users.noreply.github.com>
2021-01-21 16:59:54 -0300

    Update start.bat

--- phishing/mc.universocraft.com/start.bat
+++ phishing/mc.universocraft.com/start.bat
@@ -1,5 +1,5 @@
 @echo off
 
 title MCPTool - Phishing mc.universocraft.com
-java -Xms512M -Xmx3024M -XX:MaxPermSize=128M -jar spigot-1.8.8.jar
+java -Xms512M -Xmx1024M -XX:MaxPermSize=128M -jar spigot-1.8.8.jar
 pause
