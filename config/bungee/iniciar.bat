29cce45cc0b0d31d72cb58e0c2e24160c5842553
Pedro Vega <67062049+wrrulos@users.noreply.github.com>
2021-11-09 13:14:01 -0300

    Delete config directory

--- config/bungee/iniciar.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-title Proxy
-java -Xms512M -Xmx1024M -XX:MaxPermSize=128M -jar BungeeCord.jar
-pause
--- config/phishing/mc.universocraft.com/iniciar.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-title MCPTool - Phishing mc.universocraft.com
-java -Xms512M -Xmx1024M -XX:MaxPermSize=128M -jar spigot-1.8.8.jar
-pause
