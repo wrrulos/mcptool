564db3593c82dea86e8b34f1fa806960d468c0c2
Pedro Vega <67062049+wrrulos@users.noreply.github.com>
2022-10-19 11:32:56 -0300

    Add files via upload

---
+++ settings/scripts/Connect.js
@@ -0,0 +1,83 @@
+// Script created for MCPTool @wrrulos
+// node Connect.js <host> <port> <name> <version> [<proxyHost>] [<proxyPort>]
+
+const mineflayer = require('mineflayer')
+const socks = require('socks').SocksClient
+const ProxyAgent = require('proxy-agent')
+const readline = require('readline');
+
+if (process.argv.length < 6 || process.argv.length > 8) {
+  console.log('Usage : node Connect.js <host> <port> <name> <version> [<proxyHost>] [<proxyPort>]')
+  process.exit(1)
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const proxyHost = process.argv[6]
+const proxyPort = process.argv[7]
+
+if (proxyHost == null) {
+  var client = mineflayer.createBot({
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4],
+  version: process.argv[5],
+});
+} else {
+  var client = mineflayer.createBot({
+    connect: client => {
+      socks.createConnection({
+        proxy: {
+          host: proxyHost,
+          port: parseInt(proxyPort),
+          type: 5
+        },
+        command: 'connect',
+        destination: {
+          host: process.argv[2],
+          port: parseInt(process.argv[3])
+        }
+      }, (err, info) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+  
+        client.setSocket(info.socket)
+        client.emit('connect')
+      })
+    },
+    agent: new ProxyAgent({ protocol: 'socks5:', host: proxyHost, port: proxyPort }),
+    username: process.argv[4],
+    version: process.argv[5],
+  })
+}
+
+client.on("message",(toAnsi)=>{
+  var msg = toAnsi.toAnsi()
+  console.log(' ', msg)
+
+  rl.question('', function (command) {
+
+    if (command == '--help') {
+      console.log('help-message')
+    }
+
+    else if (command == '--exit') {
+      process.kill(process.pid)
+    }
+
+    else {
+      client.chat(command)
+    }
+      
+  });
+});
+
+client.on('login', () => { console.log('  [#] The bot connected to the server successfully. \n  Use the --help command to see the available commands.\n\n') })  // This message will be displayed if the bot enters the server
+client.on('kicked', (reason) => {  console.log('\n  [#] Disconnect: ', reason), process.kill(process.pid) })  //
+client.on('error', function (error) { console.log('\n  [#] Client Error', error), process.kill(process.pid) })
