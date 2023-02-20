9eb545ef6bb3abe0f9890c74d142a761f08f1a78
Pedro Vega <67062049+wrrulos@users.noreply.github.com>
2023-02-20 18:37:34 -0300

    Add files via upload

---
+++ utils/scanners/Nmap.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+import subprocess
+import re
+import os
+
+from utils.managers.Settings import SettingsManager
+from utils.checks.Encoding import check_encoding
+
+sm = SettingsManager()
+settings = sm.read('settings')
+
+def nmap(target, ports, file):
+    """ 
+    Scan the specified target using Nmap 
+
+    :param target: IP address
+    :param ports: Ports Range
+    :param file: Scan file
+    :return: IP List
+    """
+
+    try:
+        ip_list = []
+        command = settings['NMAP_COMMAND'].replace('[0]', ports
+                                         ).replace('[1]', file
+                                         ).replace('[2]', target)
+
+        subprocess.run(command, shell=True)
+
+        with open(file, 'r', encoding=check_encoding(file)) as f:
+            for line in f:
+                ip = re.findall('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)  # Search for an ip address in the line.
+                ip = ' '.join(ip)
+                ip = ip.replace('(', '').replace(')', '')
+
+                port = re.findall('\d{1,5}\/tcp open', line)  # Search for an open port in the line.
+                port = ' '.join(port)
+            
+                if len(port) == 0:  # If it doesn't find an open port, it tries to search for a filtered one.
+                    if settings['SHOW_NMAP_FILTERED_PORTS']:
+                        port = re.findall('\d{1,5}\/tcp filtered', line)
+                        port = ' '.join(port)
+
+                if '.' in ip:
+                    current_ip = ip
+
+                if 'tcp' in port:
+                    port = port.replace('/tcp open', '').replace('/tcp filtered', '')
+                    current_ip_backup = current_ip
+
+                    try:
+                        current_ip = current_ip.split(' ')
+                        current_ip = current_ip[1]
+
+                    except IndexError:
+                        current_ip = current_ip_backup
+
+                    ip_list.append(f'{current_ip}:{port}')
+
+            return ip_list
+    
+    except KeyboardInterrupt:
+        try:
+            os.remove(file)
+
+        except FileNotFoundError:
+            pass
+
+    return 'CtrlC'
---
+++ utils/scanners/QuboScanner.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+import subprocess
+import os
+import re
+
+from utils.managers.Settings import SettingsManager 
+from utils.checks.Encoding import check_encoding
+from utils.checks.Folder import check_folders
+
+sm = SettingsManager()
+settings = sm.read('settings')
+
+
+def quboscanner(target, ports):
+    """ 
+    Scan the specified target using QuboScanner
+
+    :param target: IP address
+    :param ports: Ports Range
+    :return: IP List
+    """
+
+    try:
+        ip_list = []
+        scan_file = None
+
+        check_folders('utils/scanners/quboscanner/outputs')
+        file_list = os.listdir('utils/scanners/quboscanner/outputs')  # Save current quboscanner outputs to a list
+        command = settings['QUBO_COMMAND'].replace('[0]', target
+                                         ).replace('[1]', ports
+                                         ).replace('[2]', settings["QUBO_THREADS"]
+                                         ).replace('[3]', settings["QUBO_TIMEOUT"])
+
+        subprocess.run(f'cd utils/scanners/quboscanner && {command}', shell=True)
+        new_file_list = os.listdir('utils/scanners/quboscanner/outputs')  # Save current quboscanner outputs back to another list
+
+        for file in new_file_list:
+            if file not in file_list:
+                scan_file = f'utils/scanners/quboscanner/outputs/{file}'
+                break
+        
+        if scan_file is None:
+            return ip_list
+
+        with open(scan_file, 'r', encoding=check_encoding(scan_file)) as f:
+            for line in f:
+                ip = re.findall('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}', line)
+                ip = ' '.join(ip)
+
+                if ':' in ip:
+                    ip_list.append(ip)
+
+        return ip_list
+    
+    except KeyboardInterrupt:
+        if scan_file is not None:
+            try:
+                os.remove(scan_file)
+
+            except FileNotFoundError:
+                pass
+
+        return 'CtrlC'
+
+
+    
