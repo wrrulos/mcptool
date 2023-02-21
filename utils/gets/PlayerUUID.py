f1230b818b1492b241da4294ac2377b5a29add6c
swasd <CustoomNPC@gmail.com>
2023-02-21 10:58:39 +0900

    fix premium player UUID format

--- utils/gets/PlayerUUID.py
+++ utils/gets/PlayerUUID.py
@@ -24,7 +24,7 @@ def player_uuid(username):
         r_json = r.json()
 
         online_uuid = r_json['id']
-        online_uuid = f'{online_uuid[0:8]}-{online_uuid[8:12]}-{online_uuid[12:16]}-{online_uuid[16:21]}-{online_uuid[21:32]}'
+        online_uuid = f'{online_uuid[0:8]}-{online_uuid[8:12]}-{online_uuid[12:16]}-{online_uuid[16:20]}-{online_uuid[20:32]}'
         offline_uuid = str(uuid.UUID(bytes=hashlib.md5(bytes(f'OfflinePlayer:{username}', 'utf-8')).digest()[:16], version=3))
         return online_uuid, offline_uuid
 
