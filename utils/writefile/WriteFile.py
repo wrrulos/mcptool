f255c46bb96ad377817061960d0d964a8beea32d
Pedro Vega <67062049+wrrulos@users.noreply.github.com>
2023-04-24 17:53:51 -0300

    v3.6

---
+++ utils/writefile/WriteFile.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+date = datetime.now()
+
+
+def WriteFile(file, clean_file, mode, *text):
+    """
+    Write the file with the specified text.
+    Also cleans it if necessary.
+
+    Parameters:
+    file (str): File name
+    clean_file (bool): Boolean value that decides whether to delete the contents of the file
+    mode (str): How to open the file
+    *text (str): Text to be written to the file
+    """
+
+    with open(file, mode=mode, encoding='utf8') as f:
+        if clean_file:
+            f.truncate(0)
+
+        for i in text:
+            f.write(str(i))
