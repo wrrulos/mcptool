29230f2a8b2fadd5db72dd5e3ef7d0a9fc2a1020
Pedro Vega <67062049+wrrulos@users.noreply.github.com>
2023-04-29 15:41:39 -0300

    Update Listening.py

--- utils/commands/Listening.py
+++ utils/commands/Listening.py
@@ -16,7 +16,7 @@ def listening_command(server):
     Saves the name and uuid of the player.
 
     Parameters:
-    server (str): IP Address and Port
+        server (str): IP Address and Port
     """
 
     players = []
