26c6bd5c497da68ad73839342d98b2a1a5856c65
Pedro Vega <67062049+wrrulos@users.noreply.github.com>
2023-07-29 06:27:29 -0300

    Update check_scan_method.py
    Change old_qubo to quboscanner

--- utils/checks/check_scan_method.py
+++ utils/checks/check_scan_method.py
@@ -9,7 +9,7 @@ def check_scan_method(method):
         bool: Returns True if the scan method is valid.
     """
 
-    methods = ['nmap', 'old_qubo', 'old_quboscanner', 'masscan', '0', '1', '2']
+    methods = ['nmap', 'qubo', 'quboscanner', 'masscan', '0', '1', '2']
 
     if method in methods:
         return True
