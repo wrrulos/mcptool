199013d6b56acd3ca45c214bd207ee57675db7eb
Pedro Vega <67062049+wrrulos@users.noreply.github.com>
2023-10-25 11:55:04 -0300

    Delete utils directory

--- utils/commands/reload_command.py
+++
@@ -1,9 +0,0 @@
-from utils.color.text_color import paint
-from utils.gets.get_spaces import get_spaces
-from utils.managers.config_manager import config_manager
-from utils.managers.language_manager import language_manager
-
-
-def reload_command():
-    config_manager.load_config()
-    paint(f'\n{get_spaces()}{language_manager.language["prefix"]}{language_manager.language["commands"]["reload"]["reloading"]}')
