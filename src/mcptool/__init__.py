37f8f2f55ca12ad0e93dfa70f0dd00909099b140
Pedro Agustin Vega <vegapedroagustin@gmail.com>
2024-03-29 23:05:53 -0300

    Add new logger module (loguru)

--- src/mcptool/__init__.py
+++ src/mcptool/__init__.py
@@ -1,25 +1,42 @@
-import logging
+import os
+
+from loguru import logger
+
+# Import the MCPToolPath class
+from .modules.utilities.path.mcptool_path import MCPToolPath
+
+# Remove the default logger
+logger.remove()
+
+# Set the logging configuration
+logger.add(os.path.join(MCPToolPath().get(), 'debug.log'),
+    level='INFO',
+    format='[{time} {level} - {file}, {line}] ⮞ <level>{message}</level>', 
+    rotation="50 MB"
+)
 
 from mccolors import mcwrite, mcreplace
 
+# Utilities
 from .modules.utilities.managers.language_manager import LanguageManager as LM
-from .modules.utilities.commands.loader import CommandLoader
 
 
 class MCPTool:
+    __version__: str = '1.0-alpha'
+    
     def __init__(self, commands_folder_path: str = 'src/mcptool/modules/commands'):
         self.commands_folder_path = commands_folder_path
         self.commands: dict = {}
 
     def run(self):
-        # Set the logging configuration
-        logging.basicConfig(filename='debug.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-        
+        # Check if the files exist
+        MCPToolPath().check_files()
+
         # Notify the user that the tool is starting
-        logging.info(LM().get(['logger', 'starting']))
+        logger.info(LM().get(['logger', 'starting']))
 
         # Load the commands
-        self.commands = CommandLoader(commands_folder_path=self.commands_folder_path).get_commands()
+        self.commands = self._get_commands()
 
         # Start the command input
         self._command_input()
@@ -28,11 +45,11 @@ class MCPTool:
         """
         Method to manage the command line input
         """
-
+        
+        print(MCPToolPath().get())
         while True:
             try:
-                # arguments = input(mcreplace(LM().get(['commands', 'input']))).split()
-                arguments: list = input(mcreplace('Enter a command: ')).split()
+                arguments = input(mcreplace(LM().get(['commands', 'input']))).split()
 
                 if len(arguments) == 0:
                     continue
@@ -62,3 +79,23 @@ class MCPTool:
 
             except KeyboardInterrupt:
                 break
+
+    def _get_commands(self) -> dict:
+        """
+        Method to get the commands
+
+        Returns:
+            dict: The commands
+        """
+
+        # Commands
+        from .modules.commands.server import Command as ServerCommand
+        from .modules.commands.player import Command as PlayerCommand
+        from .modules.commands.ipinfo import Command as IPInfoCommand
+
+        return {
+            'server': ServerCommand(),
+            'player': PlayerCommand(),
+            'ipinfo': IPInfoCommand()
+        }
+
