9c391a9b9dcd2db0e51632732ae9ecd9387ded00
Pedro Agustin Vega <vegapedroagustin@gmail.com>
2024-04-28 08:36:56 -0300

    Add threads in subdomains command

--- src/mcptool/modules/commands/subdomains.py
+++ src/mcptool/modules/commands/subdomains.py
@@ -1,3 +1,4 @@
+import threading
 import socket
 import time
 import os
@@ -6,6 +7,7 @@ from loguru import logger
 from mccolors import mcwrite
 
 from ..utilities.managers.language_manager import LanguageManager as LM
+from ..utilities.managers.settings_manager import SettingsManager as SM
 from ..utilities.commands.validate import ValidateArgument
 
 
@@ -49,54 +51,104 @@ class Command:
         Args:
             arguments (list): The arguments to execute the command
         """
-
-        # Validate the arguments
+        
         if not self.validate_arguments(arguments):
             return
-        
-        domain: str = arguments[0]
-        file: str = arguments[1]
-        subdomains: int = 0  # Counter for the subdomains found
 
-        # Read the file
-        with open(file, 'r') as f:
-            subdomain_list: list = f.readlines() 
+        domain: str = arguments[0]
+        file_path: str = arguments[1]
+        subdomains_found: int = 0
+        results: list = []
+
+        try:
+            num_threads: int = int(SM().get('subdomains_threads'))
+
+        except ValueError:
+            logger.warning(f'Invalid value for subdomains_threads. Using default value (500)')
+            num_threads: int = 500
+
+        with open(file_path, 'r') as file:
+            subdomain_list = [line.strip() for line in file]
 
         if len(subdomain_list) == 0:
             mcwrite(LM().get(['errors', 'subdomainsFileEmpty']))
             return
-
+        
         mcwrite(LM().get(['commands', self.name, 'wordlist'])
-            .replace('%file%', file)
+            .replace('%file%', file_path)
             .replace('%subdomains%', str(len(subdomain_list)))
         )
         time.sleep(0.5)
         mcwrite(LM().get(['commands', self.name, 'gettingSubdomains']))
 
-        # Check the lines
-        for subdomain in subdomain_list:
-            try:
-                host = f'{subdomain}.{domain}'
-                ip = socket.gethostbyname(host)
-                
-                # Display information about the resolved subdomain.
-                mcwrite(LM().get(['commands', self.name, 'subdomainFound'])
-                    .replace('%subdomain%', host)
-                    .replace('%ip%', ip)
-                )
-                    
-                # Increment the subdomains counter.
-                subdomains += 1
+        # Check if the number of threads is greater than the number of subdomains
+        if len(subdomain_list) < num_threads:
+            num_threads = len(subdomain_list)
 
-            except socket.gaierror:
-                continue
+        # Divide the subdomains into chunks
+        chunk_size = len(subdomain_list) // num_threads
+        chunks = [subdomain_list[i:i+chunk_size] for i in range(0, len(subdomain_list), chunk_size)]
 
-        if subdomains == 0:
-            mcwrite(LM().get(['commands', self.name, 'noSubdomains'])
-                .replace('%file%', file)
+        # Create the threads
+        threads: list = []
+
+        # Scan the subdomains
+        for chunk in chunks:
+            thread = threading.Thread(target=self._scan_chunk, args=(domain, chunk, results))
+            threads.append(thread)
+            thread.start()
+
+        # Wait for all threads to finishs
+        for thread in threads:
+            thread.join()
+
+        for subdomain, ip in results:
+            mcwrite(LM().get(['commands', self.name, 'subdomainFound'])
+                .replace('%subdomain%', subdomain)
+                .replace('%ip%', ip)
             )
-            return
-        
-        mcwrite(LM().get(['commands', self.name, 'subdomainsFound'])
-            .replace('%subdomains%', str(subdomains))
-        )
+            subdomains_found += 1
+
+        if subdomains_found == 0:
+            mcwrite(LM().get(['commands', self.name, 'noSubdomains'])
+                .replace('%file%', file_path)
+            )
+            
+        else:
+            mcwrite(LM().get(['commands', self.name, 'subdomainsFound'])
+                .replace('%subdomains%', str(subdomains_found))
+            )
+
+    @logger.catch
+    def _scan_subdomain(self, domain: str, subdomain: str, results: list) -> None:
+        """
+        Method to scan a subdomain. 
+        If the subdomain is valid, it will be added to the results list
+
+        Args:
+            domain (str): The domain
+            subdomain (str): The subdomain
+            results (list): The results list
+        """
+
+        try:
+            host = f'{subdomain}.{domain}'
+            ip = socket.gethostbyname(host)
+            results.append((host, ip))
+
+        except socket.gaierror:
+            pass
+
+    @logger.catch
+    def _scan_chunk(self, domain: str, chunk: list, results: list) -> None:
+        """
+        Method to scan a chunk of subdomains.
+
+        Args:
+            domain (str): The domain
+            chunk (list): The chunk of subdomains
+            results (list): Results list to store the valid subdomains
+        """
+
+        for subdomain in chunk:
+            self._scan_subdomain(domain, subdomain, results)
