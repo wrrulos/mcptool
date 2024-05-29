87e403818ad05e7084d5a11327772828017b6dd8
Pedro Vega <vegapedroagustin@gmail.com>
2024-05-28 22:46:53 -0300

    Beta version

--- src/mcptool/modules/utilities/banners/banners.py
+++ src/mcptool/modules/utilities/banners/banners.py
@@ -1,7 +1,7 @@
 from ..managers.settings_manager import SettingsManager as SM
 from ..managers.language_manager import LanguageManager as LM
 
-from ..constants import SPACES, OS_NAME, DISCORD_LINK
+from ..constants import SPACES, OS_NAME, DISCORD_LINK, VERSION
 from ..constants.update_available import UPDATE_AVAILABLE
 
 
@@ -13,7 +13,7 @@ class MCPToolBanners:
 &f&l     dMP dMP dMP dMP.aMP dMP&c&l       dMP   dMP.aMP dMP.aMP dMP
 &f&l    dMP dMP dMP  VMMMP" dMP&c&l       dMP    VMMMP"  VMMMP" dMMMMMP
 
-{LM().get(['app', 'description']).replace('%version%', SM().get('version'))}{LM().get(['app', 'newVersion']) if UPDATE_AVAILABLE  else ''}'''
+{LM().get(['app', 'description']).replace('%version%', VERSION)}{LM().get(['app', 'newVersion']) if UPDATE_AVAILABLE  else ''}'''
 
     BANNER_2: str = f'''
 &f&l    ███╗   ███╗ ██████╗██████╗ &c&l████████╗ ██████╗  ██████╗ ██╗
@@ -23,7 +23,7 @@ class MCPToolBanners:
 &f&l    ██║ ╚═╝ ██║╚██████╗██║     &c&l   ██║   ╚██████╔╝╚██████╔╝███████╗
 &f&l    ╚═╝     ╚═╝ ╚═════╝╚═╝     &c&l   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
 
-{LM().get(['app', 'description']).replace('%version%', SM().get('version'))}{LM().get(['app', 'newVersion']) if UPDATE_AVAILABLE else ''}'''
+{LM().get(['app', 'description']).replace('%version%', VERSION)}{LM().get(['app', 'newVersion']) if UPDATE_AVAILABLE else ''}'''
 
     BANNER_3: str = f'''
 &f&l    8888ba.88ba   a88888b.  888888ba  &c&ld888888P                   dP
@@ -33,7 +33,7 @@ class MCPToolBanners:
 &f&l    88   88   88 Y8.   .88  88        &c&l   88    88.  .88 88.  .88 88
 &f&l    dP   dP   dP  Y88888P'  dP        &c&l   dP    `88888P' `88888P' dP
 
-{LM().get(['app', 'description']).replace('%version%', SM().get('version'))}{LM().get(['app', 'newVersion']) if UPDATE_AVAILABLE else ''}'''
+{LM().get(['app', 'description']).replace('%version%', VERSION)}{LM().get(['app', 'newVersion']) if UPDATE_AVAILABLE else ''}'''
 
     BANNER_4: str = f'''
 &f&l    `7MMM.     ,MMF' .g8"""bgd `7MM"""Mq. &c&lMMP""MM""YMM              `7MM
@@ -44,7 +44,7 @@ class MCPToolBanners:
 &f&l      M  `YM'   MM `Mb.     ,'   MM       &c&l     MM YA.   ,A9 YA.   ,A9 MM
 &f&l    .JML. `'  .JMML. `"bmmmd'  .JMML.     &c&l   .JMML.`Ybmd9'   `Ybmd9'.JMML.
 
-{LM().get(['app', 'description']).replace('%version%', SM().get('version'))}{LM().get(['app', 'newVersion']) if UPDATE_AVAILABLE else ''}'''
+{LM().get(['app', 'description']).replace('%version%', VERSION)}{LM().get(['app', 'newVersion']) if UPDATE_AVAILABLE else ''}'''
 
     BANNERS: list = [BANNER_1, BANNER_2, BANNER_3, BANNER_4]
 
@@ -74,30 +74,28 @@ class HelpBanners:
     &c• &f&lCommands:                                                          &d       ⢀⡴⠋⠁  ⠊         ⠉⠳⢦⡀
                                                                          &d     ⢀⡴⠃⢀⡴⢳               ⠙⣆
       &d► &f&lserver [ip:port/domain]                                          &d     ⡾⠁⣠⠋ ⠈⢧               ⠈⢧
-      &d► &f&lplayer [username]                                                &d    ⣸⠁⢰⠃   ⠈⢣⡀              ⠈⣇
+      &d► &f&luuid [username/UUID]                                             &d    ⣸⠁⢰⠃   ⠈⢣⡀              ⠈⣇
       &d► &f&lipinfo [ip]                                                      &d    ⡇ ⡾⡀    ⣀⣹⣆⡀             ⢹
-      &d► &f&lreverseip [ip]                                                   &d   ⢸⠃⢀⣇⡈      &d⢀⡑⢄⡀⢀⡀         ⢸⡇
-      &d► &f&ldnslookup [domain]                                               &d   ⢸ &f&l⢻⡟⡻⢶⡆   ⡼⠟⡳⢿⣦&d⡑⢄         ⢸⡇
-      &d► &f&lsearch [data]                                                    &d   ⣸ &f&l⢸⠃⡇⢀⠇     ⡼  ⠈⣿&d⡗⠂       ⢸⠁
-      &d► &f&lwebsearch [tag] [bot] <proxy>                                    &d   ⡏ &f&l⣼ ⢳⠊      ⠱⣀⣀⠔&d⣸⠁       ⢠⡟
-      &d► &f&laternos [pages] [bot] <proxy>                                    &d   ⡇&f&l⢀⡇           ⠠ &d⡇        ⢸⠃
-      &d► &f&lscan [ip] [ports] [method] [bot] <proxy>                         &d  ⢸⠃&f&l⠘⡇            &d⢸⠁  ⢀     ⣾
-      &d► &f&lchecker [file] [bot] <proxy>                                     &d  ⣸  &f&l⠹⡄  ⠈⠁       &d⡞   ⠸     ⡇
-      &d► &f&llistening [ip:port/domain]                                       &d  ⡏   &f&l⠙⣆       ⢀⣠⢶&d⡇  ⢰⡀     ⡇
-      &d► &f&lplayerlogs [ip:port/domain]                                      &d ⢰⠇⡄    &f&l⢣⣀⣀⣀⡤⠴⡞⠉ &d⢸   ⣿⡇     ⣧
-      &d► &f&lbungee [ip:port/domain]                                          &d ⣸ ⡇       &f&l   ⢹  &d⢸  ⢀⣿⠇   ⠁ ⢸
-      &d► &f&lvelocity [ip:port/domain] [forwarding-mode]                      &d ⣿ ⡇    &f&l ⢀⡤⠤⠶⠶⠾⠤⠄&d⢸ ⡀⠸⣿⣀     ⠈⣇
-      &d► &f&lfakeproxy [ip:port/domain] [forwarding-mode]                     &d ⡇ ⡇    &f&l⡴⠋       &d⠸⡌⣵⡀⢳⡇      ⢹⡀
-      &d► &f&lconnect [ip:port] [username] [version] <proxy>                   &d ⡇ ⠇   &f&l⡸⠁         &d⠙⠮⢧⣀⣻⢂      ⢧
-      &d► &f&lrconnect [ip:rconPort] [passwordFile]                            &d ⣇ ⢠   &f&l⠇            &d  ⠈⡎⣆     ⠘
-      &d► &f&lrcon [ip:rconPort] [passwordFile] <delay>                        &d ⢻ ⠈⠰ &f&l⢸             &d  ⠰⠘⢮⣧⡀
-      &d► &f&lauthme [ip:port] [username] [version] [passwordFile] <proxy>     &d ⠸⡆  ⠇&f&l⣾             &d     ⠙⠳⣄⡀⡀
-      &d► &f&lkick [ip:port] [username] [version] [loop] <proxy>               &d ⠸⡆  ⠇&f&l⣾             &d       ⠙⠳⢢
-      &d► &f&lkickall [ip:port] [version] [loop] <proxy>                       &d ⠸⡆  ⠇&f&l⣾             &d        ⠙⣄⢢
-      &d► &f&lsendcmd [ip:port] [username] [version] [file] [loop] <proxy>     &d ⠸⡆  ⠇&f&l⣾             &d         ⠙⡀⢢
-      &d► &f&llanguage [language]                                              &d ⠸⡆  ⠇&f&l⣾             &d         ⠙⡀⢢'''
+      &d► &f&ldnslookup [domain]                                               &d   ⢸⠃⢀⣇⡈      &d⢀⡑⢄⡀⢀⡀         ⢸⡇
+      &d► &f&lchecker [file]                                                   &d   ⢸ &f&l⢻⡟⡻⢶⡆   ⡼⠟⡳⢿⣦&d⡑⢄         ⢸⡇
+      &d► &f&lresolver [domain]                                                &d   ⣸ &f&l⢸⠃⡇⢀⠇     ⡼  ⠈⣿&d⡗⠂       ⢸⠁
+      &d► &f&lseeker [token/servers]                                           &d   ⡏ &f&l⣼ ⢳⠊      ⠱⣀⣀⠔&d⣸⠁       ⢠⡟
+      &d► &f&lsubdomains [domain] [wordlist]                                   &d   ⡇&f&l⢀⡇           ⠠ &d⡇        ⢸⠃
+      &d► &f&lscan [ip_address/ip_range] [port/port_range] [method             &d  ⢸⠃&f&l⠘⡇            &d⢸⠁  ⢀     ⣾
+      &d► &f&llistening [ip:port]                                              &d  ⣸  &f&l⠹⡄  ⠈⠁       &d⡞   ⠸     ⡇
+      &d► &f&lproxy [ip:port] [proxy_type]                                     &d  ⡏   &f&l⠙⣆       ⢀⣠⢶&d⡇  ⢰⡀     ⡇
+      &d► &f&lfakeproxy [ip:port] [velocity_forwading_mode]                    &d ⢰⠇⡄    &f&l⢣⣀⣀⣀⡤⠴⡞⠉ &d⢸   ⣿⡇     ⣧
+      &d► &f&lconnect [ip:port] [version] [username]                           &d ⣸ ⡇       &f&l   ⢹  &d⢸  ⢀⣿⠇   ⠁ ⢸
+      &d► &f&lsendcmd [ip:port] [version] [username] [commands_file]           &d ⣿ ⡇    &f&l ⢀⡤⠤⠶⠶⠾⠤⠄&d⢸ ⡀⠸⣿⣀     ⠈⣇
+      &d► &f&lrcon [ip:port] [password]                                        &d ⡇ ⡇    &f&l⡴⠋       &d⠸⡌⣵⡀⢳⡇      ⢹⡀
+      &d► &f&lbrutercon [ip:port] [passwords_file]                             &d ⡇ ⠇   &f&l⡸⠁         &d⠙⠮⢧⣀⣻⢂      ⢧
+      &d► &f&lbruteauth [ip:port] [version] [username] [passwords_file]        &d ⣇ ⢠   &f&l⠇            &d  ⠈⡎⣆     ⠘
+      &d► &f&lkick [ip:port] [version] [username] [loop]                       &d ⢻ ⠈⠰ &f&l⢸             &d  ⠰⠘⢮⣧⡀
+      &d► &f&lkickall [ip:port] [version] [loop]                               &d ⠸⡆  ⠇&f&l⣾             &d     ⠙⠳⣄⡀⡀
+      &d► &f&llanguage [language]  (not available in beta)                     &d ⠸⡆  ⠇&f&l⣾             &d       ⠙⠳⢢
+      &d► &f&lclear                                                            &d ⠸⡆  ⠇&f&l⣾             &d         ⠙⡀⢢'''
 
-    BANNERS: list = [HELP_BANNER_1, HELP_BANNER_2]
+    BANNERS: list = [HELP_BANNER_2, HELP_BANNER_2]
 
 class DiscordBanners:
     DISCORD_BANNER_1 = f'''
